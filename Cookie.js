1a50e79ac7238d7520ab9ad3ac6ebe70eecdc4b0
5tudioDigital <online@studiodigital.at>
2021-03-29 16:12:54 +0200

    Update Cookie.js

--- Cookie.js
+++ Cookie.js
@@ -1,10 +1,3 @@
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PT4V7D6');
-
-
 var Webflow = Webflow || [];
 function getCookie(e) {
     for (var t = e + "=", o = document.cookie.split(";"), c = 0; c < o.length; c++) {
